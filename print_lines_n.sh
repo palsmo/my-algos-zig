6f3087c6aa1761d044cf73c322ba178a4d49cea5
palsmo <johannesedvingreen@gmail.com>
2024-08-30 16:15:34 +0200

    update

--- print_lines_n.sh
+++ print_lines_n.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# find all .zig files and count their lines
+# find all .zig files and sum their line count, print
 total_lines=$(find . -name "*.zig" -type f -not -path "*/.zig-cache/*" | xargs wc -l | tail -n 1 | awk '{print $1}')
-
 echo "Lines of Code: ${total_lines}";
